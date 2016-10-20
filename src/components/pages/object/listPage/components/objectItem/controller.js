edde3831c4e5e26a6a696199a25619fe15158aae
Shvetcov <vikshv@yandex.ru>
2016-10-20 21:22:25 +0300

    object card

---
+++ src/components/pages/object/listPage/components/objectItem/controller.js
@@ -0,0 +1,5 @@
+export default class ObjectItemController {
+    constructor() {
+        'ngInject';
+    }
+};
--- src/components/pages/object/listPage/index.js
+++ src/components/pages/object/listPage/index.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import component from './component';
+import objectItem from './components/objectItem';
 
 export default angular.module('app.components.pages.object.list', [
+        objectItem.name
     ])
     .component('objectListPage', component);
