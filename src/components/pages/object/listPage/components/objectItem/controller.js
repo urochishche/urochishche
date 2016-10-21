9e661b649f57db59393959479996c29a74eb787d
Shvetcov <vikshv@yandex.ru>
2016-10-21 10:36:46 +0300

    object card for list of objects

--- src/components/pages/object/listPage/components/objectItem/controller.js
+++ src/components/pages/object/listPage/components/objectItem/controller.js
@@ -2,4 +2,17 @@ export default class ObjectItemController {
     constructor() {
         'ngInject';
     }
+
+    _getVisitorsText() {
+        const { visitors } = this.object;
+        let result;
+        if (visitors === 1) {
+            result = 'посещение';
+        } else if (visitors < 5) {
+            result = 'посещения';
+        } else {
+            result = 'посещений';
+        }
+        return result;
+    }
 };
